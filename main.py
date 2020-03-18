57eba4af74451ad895435119d77d5f8549297f45
Polina Vinogradova <p.p.vinogradova@yandex.ru>
2020-03-18 15:51:53 +0000

    iterator done

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import json
-from pprint import pprint
+import wikipedia
 
-# class CountriesWiki:
+def make_list(file):
+  with open(file,'r') as f:
+    info = json.load(f)
+    with open('countries_list.txt', 'w') as f:
+      for c in info:
+        country = c['name']['common']
+        c = country + '\n'
+        f.write(c)
 
-#   def __init__(self, path):
-#     self.path = path
-#     self.file = open(self.path, encoding = 'utf8')
+class CountriesWiki:
+
+  def __init__(self, path):
+    self.file = open(path, encoding = 'utf-8')
+
+  def __iter__(self):
+    return self
+
+  def __next__(self):
+    line = self.file.readline().strip()
+    page_id = line + '(country)'
+    try:
+      page = wikipedia.page(page_id)
+      wiki_url = page.url
+    except wikipedia.exceptions.PageError:
+      wiki_url = 'no page found'
+    finally:
+      string = line + ': ' + wiki_url + '\n'
+      if not line:
+        raise StopIteration
+      return string
+
+if __name__ == '__main__':
+  make_list('countries.json')
+  with open('results.txt', 'w') as f:
+    for result in CountriesWiki('countries_list.txt'): 
+      f.write(result)
+      print('.')
   
-
-
-with open('countries.json') as f:
-  info = json.load(f)
-  for c in info:
-    country = c['name']['common']
-
-
-  #f'https://ru.wikipedia.org/wiki/{country}'
